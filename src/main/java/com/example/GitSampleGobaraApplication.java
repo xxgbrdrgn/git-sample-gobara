e3161bb5b5ea76ac57995c2fbad105758e65a890
xxgbrdrgn <xxgbrdrgn@outlook.jp>
2026-05-19 11:34:55 +0900

    新規ファイル作成

---
+++ src/main/java/com/example/GitSampleGobaraApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleGobaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleGobaraApplication.class, args);
+	}
+
+}
